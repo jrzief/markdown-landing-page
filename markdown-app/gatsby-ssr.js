9e366faef09ba1a7d165db77307ececd4169c3a1
Joel <joelrzief@gmail.com>
2020-02-10 22:28:42 -0500

    Initial Gatsby setup

---
+++ markdown-app/gatsby-ssr.js
@@ -0,0 +1,15 @@
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
+
+// You can delete this file if you're not using it
+
+import React from "react"
+import { ThemeProvider } from "theme-ui"
+import theme from "@rebass/preset"
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={theme}>{element}</ThemeProvider>
+)

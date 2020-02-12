fe351bc02b015a475f977d1a383c4a6b98c93982
Joel <joelrzief@gmail.com>
2020-02-12 10:12:40 -0500

    Setup of auth0 hook completed

--- markdown-app/gatsby-ssr.js
+++ markdown-app/gatsby-ssr.js
@@ -7,9 +7,22 @@
 // You can delete this file if you're not using it
 
 import React from "react"
-import { ThemeProvider } from "theme-ui"
-import theme from "@rebass/preset"
+import { navigate } from "gatsby"
+import { AuthProvider } from "react-use-auth"
 
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={theme}>{element}</ThemeProvider>
+  <AuthProvider
+    navigate={navigate}
+    auth0_domain="markdown-landing-page.auth0.com"
+    auth0_client_id="Kflyw0Tx3FGSDBtEGoNjiruBJwI2snan"
+  >
+    {element}
+  </AuthProvider>
 )
+
+//import { ThemeProvider } from "theme-ui"
+//import theme from "@rebass/preset"
+
+//export const wrapRootElement = ({ element }) => (
+//<ThemeProvider theme={theme}>{element}</ThemeProvider>
+//)
--- markdown-app/src/pages/index.js
+++ markdown-app/src/pages/index.js
@@ -5,17 +5,28 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+import { Heading, Button } from "rebass"
+
+import { useAuth } from "react-use-auth"
+
+const IndexPage = () => {
+  const { isAuthenticated, user, login } = useAuth()
+  return (
+    <Layout>
+      <SEO title="Markdown Landing Page" />
+      <Heading fontSize={[5, 6, 7]}> Markdown Landing Page </Heading>
+      <h1>Hi people</h1>
+      <p>Write a landing page for anything, in all situtions.</p>
+      {isAuthenticated() ? <p>Hello {user.name}</p> : null}
+      <Button bg="highlite" onClick={login}>
+        Get Started
+      </Button>
+      {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> */}
+    </Layout>
+  )
+}
 
 export default IndexPage

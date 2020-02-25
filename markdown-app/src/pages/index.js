795f0b58b0614e8e2c4a4b34c1c3b8da8f2b8884
Joel <joelrzief@gmail.com>
2020-02-24 22:36:07 -0500

    Login Button calls updateUser mutation on backend

--- markdown-app/src/pages/index.js
+++ markdown-app/src/pages/index.js
@@ -1,20 +1,57 @@
-import React from "react"
-import { useStaticQuery, graphql, Link } from "gatsby"
+import React, { useEffect } from "react"
+//import { useStaticQuery, graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+//import Image from "../components/image"
 import SEO from "../components/seo"
 
 import { Heading, Button } from "rebass"
 
 import { useAuth } from "react-use-auth"
+import { useMutation } from "react-apollo-hooks"
 
-import { useQuery } from "react-apollo-hooks"
+//import { useQuery } from "react-apollo-hooks"
 import gql from "graphql-tag"
+
+const LoginButton = () => {
+  const { isAuthenticated, user, userId, login, logout } = useAuth()
+
+  const [updateUser, { data }] = useMutation(
+    gql`
+      mutation updateUser($userId: String) {
+        updateUser(userId: $userId) {
+          userId
+        }
+      }
+    `,
+    {
+      variables: {
+        userId,
+      },
+    }
+  )
+  // runs when userId updates
+  useEffect(() => {
+    updateUser()
+  }, [userId])
+
+  console.log(data)
+
+  return isAuthenticated() ? (
+    <Button bg="muted" onClick={logout}>
+      Logout
+    </Button>
+  ) : (
+    <Button bg="highlight" onClick={login}>
+      Get started
+    </Button>
+  )
+}
 
 const IndexPage = () => {
   const { isAuthenticated, user, login } = useAuth()
-  const data = useStaticQuery(graphql`
+
+  /* const data = useStaticQuery(graphql`
     query {
       mdlapi {
         hello {
@@ -30,25 +67,27 @@ const IndexPage = () => {
         world
       }
     }
-  `)
+  `) */
 
-  const liveData = apolloData.data,
-    loading = apolloData.loading
+  /*  const liveData = apolloData.data,
+    loading = apolloData.loading */
   return (
     <Layout>
       <SEO title="Markdown Landing Page" />
       <Heading fontSize={[5, 6, 7]}> Markdown Landing Page </Heading>
       <h1>Hi people</h1>
       <p>Write a landing page for anything, in all situtions.</p>
-      <p>
+      {/* <p>
         From GraphQL server:{" "}
         {liveData ? liveData.hello.world : data.mdlapi.hello.world}
-      </p>
-      {loading ? <p>Fetching ..</p> : null}
+      </p> */}
+      {/* {loading ? <p>Fetching ..</p> : null} */}
       {isAuthenticated() ? <p>Hello {user.name}</p> : null}
-      <Button bg="highlite" onClick={login}>
+
+      <LoginButton />
+      {/* <Button bg="highlite" onClick={login}>
         Get Started
-      </Button>
+      </Button> */}
       {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
